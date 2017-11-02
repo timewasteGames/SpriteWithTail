102e27210619c835827ca7ec9722003eab90ca85
timewastegames <timewastegames@gmail.com>
2017-11-02 14:53:19 +0000

    Default libGDX project.

---
+++ desktop/src/com/brussell/spritewithtail/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.brussell.spritewithtail.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.brussell.spritewithtail.SpriteWithTail;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpriteWithTail(), config);
+	}
+}
